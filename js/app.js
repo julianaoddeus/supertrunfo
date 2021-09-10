ce4847839d14c3dd4f5961050622bc963b9795b1
Juliana Oliveira <julianaacademi@gmail.com>
2021-09-09 21:24:23 -0300

    Inserido function Pegar Atributos

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+// Criado os objetos CARTA com seus atributos.
 const naruto = {
     nome: "Naruto",
     imagem: "img/naruto.png",
@@ -19,23 +20,60 @@ const batman = {
 }
 
 const cartas = [naruto, batman];
+let cartaJogador, cartaMaquina;
 
+
+// Criado função de Sortear as cartas
 function SortearCarta(){
     const numeroDeCartas = 2;
     let numeroCartaJogador = parseInt(Math.random()*numeroDeCartas);
-    let numeroCartaComputador = parseInt(Math.random()*numeroDeCartas);
+    let numeroCartaMaquina = parseInt(Math.random()*numeroDeCartas);
     
     // Laço para não repetir as cartas entres os competidores
-    while (numeroCartaJogador == numeroCartaComputador){
+    while (numeroCartaJogador == numeroCartaMaquina){
         numeroCartaJogador = parseInt(Math.random()*numeroDeCartas);
     }
-    const cartaJogador = cartas[numeroCartaJogador];
-    const cartaComputador = cartas[numeroCartaComputador];
-    console.log(cartaJogador , cartaComputador);
+    
+    cartaJogador = cartas[numeroCartaJogador];
+    cartaMaquina = cartas[numeroCartaMaquina];
+    
+    
 }
 
-const btnSortear = document.querySelector("#btnSortear").onclick = () => {
+// Exibir a carta do Jogador e carta Maquina
+function ExibirCartaJogador(){
+    console.log(cartaJogador);
+    let divCartaJogador = document.querySelector("#carta-jogador");
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+    document.querySelector(".nome-personagem-jogador").innerText = cartaJogador.nome;
+
+    //pegar e listar atributos para
+    let listaDeAtributosDaCarta = document.querySelector(".atributos-jogador");
+    listaDeAtributosDaCarta.innerHTML = PegarAtributos(cartaJogador.atributos);
+    
+    
+}
+
+function PegarAtributos(atributosDaCarta){
+    let listaDeAtributos = "", botaoRadio="";
+    for(let atributo in atributosDaCarta){
+        botaoRadio = ` <input type="radio" name="atributo" value="${atributo}">`;
+        listaDeAtributos += `<li> ${atributo} : ${atributosDaCarta[atributo]} ${botaoRadio}</li>`;
+    }
+    return listaDeAtributos;
+}
+
+
+
+// Inserindo evento no botão SORTEAR_CARTA.
+const btnSortear = document.querySelector("#btnSortear");
+btnSortear.onclick = () => {
     SortearCarta();
+    ExibirCartaJogador();
+    document.querySelector("#btnJogar").disabled = false;
 };
+
+
+
 
 
